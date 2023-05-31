4748a121c1f7ef0321b93ce60b6a81ec286bb119
Álvaro Prudêncio <43001689+im-a-Cloud@users.noreply.github.com>
2023-05-31 20:54:39 -0300

    Add files via upload

--- src/main/java/com/VascoBankCartao/VascoBankCartaoart/models/CartaoCredito.java
+++ src/main/java/com/VascoBankCartao/VascoBankCartaoart/models/CartaoCredito.java
@@ -10,5 +10,54 @@ public class CartaoCredito {
             boolean eligivelAumentoLimite;
             boolean virtual;
 
+    public void setId(int idConta) {
+        id = idConta;
+    }
+    public int getId(){
+        return id;
+    }
 
+    public void setNome(String nomeConta) {
+        nome = nomeConta;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNumero(int numeroConta) {
+        numero = numeroConta;
+    }
+    public int getNumero(){
+        return numero;
+    }
+    public void setDataVencimento(String data) {
+        dataVencimento = data;
+    }
+    public String getDataVencimento(){
+        return dataVencimento;
+    }
+    public void setCvv(int cvv1) {
+        cvv = cvv1;
+    }
+    public int getCvv(){
+        return cvv;
+    }
+    public void setLimite(double limiteCartao) {
+        limite = limiteCartao;
+    }
+    public double getLimite(){
+        return limite;
+    }
+    public void setEligivelAumentoLimite(boolean podeAumentarLimite) {
+        eligivelAumentoLimite = podeAumentarLimite;
+    }
+    public boolean isEligivelAumentoLimite(){
+        return eligivelAumentoLimite;
+    }
+    public void setVirtual(boolean cartaoEhVirtual) {
+        virtual = cartaoEhVirtual;
+    }
+    public boolean isVirtual(){
+        return virtual;
+    }
 }
+
